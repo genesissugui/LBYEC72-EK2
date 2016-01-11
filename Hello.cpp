4137106956d732e304ff940b9e8539b8022b04cb
genesissugui <ginsugui@yahoo.com>
2016-01-11 13:22:27 +0800

    First Practice...

---
+++ Hello.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+void printNameCourse();
+float computeCircleArea(float functionRadius);
+
+int main()
+
+{
+	printNameCourse();
+	
+	/*****************/
+	float area;
+	float radius;
+	
+	radius = 10;
+	
+	area = computeCircleArea(radius);
+	
+	printf("The area is %f\n", area);
+	
+	return EXIT_SUCCESS;
+}
+
+void printNameCourse(){
+	
+	puts("Genesis Israel N. Sugui");
+	puts("Hello LBYEC72");
+}
+
+float computeCircleArea(float functionRadius){
+	
+	return PI*functionRadius*functionRadius;
+}
